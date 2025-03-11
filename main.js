a89a6b9819f02a3e6d38148dc53399a74e4c572c
Emily <145925732+EmilyGraceLB@users.noreply.github.com>
2025-03-11 15:49:09 +0000

    updated js

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 console.log("hello");
+
+const submitBtn = document.getElementById("submit");
+
+function eventHandler (event) {
+
+}
+
+submitBtn.addEventListener("click", function (event) {
+    // Function contents go here
+  });
